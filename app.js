1c44bcd2cdb0b47bcf1545365b302141f3221b68
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2025-12-25 17:35:34 +0300

    Initialize Express app

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('node:path');
+
+const app = express();
+
+// App settings
+app.use(express.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+const assetsPath = path.join(__dirname, 'public');
+app.use(express.static(assetsPath));
+
+// routes
+app.get('/', (req, res) => {});
+
+// Server
+const PORT = 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Express app - listening on port ${PORT}!`);
+});
