eeeb49cef59367c8c0670a5948b4b7887cfa9331
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2026-01-07 10:36:04 +0300

    Prepare for deployment

--- db/queries.js
+++ db/queries.js
@@ -1,18 +1,5 @@
 const pool = require('./pool');
 
-// async function getAll() {
-//     const series = [];
-//     const { rows } = await pool.query('SELECT * FROM series');
-//     for (const row of rows) {
-//       const genre = await pool.query(
-//         `SELECT genre_id, genre_name FROM series_genre JOIN genre ON genre_id = genre.id WHERE series_id = ${row.id}`,
-//       );
-//       row.genre = genre.rows;
-//       series.push(row);
-//     }
-//     return series;
-// }
-
 async function getAll() {
   const { rows } =
     await pool.query(`SELECT s.id, s.title, s.chapter_count, s.status, s.publisher, s.author, s.image_link, s.release_date, json_agg(g.*) AS genre
